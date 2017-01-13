6862b530bbdb8a096c4b23d7dbc27298840ef212
Patrick Laughrea <p.laughrea@gmail.com>
2017-01-12 22:33:11 -0500

    Refactoring and improvements
    One bug was found and fixed in multiline text. Text at the top should eventually be put in doc.

--- WebssonParser/utilsSweepers.cpp
+++ WebssonParser/utilsSweepers.cpp
@@ -5,64 +5,48 @@
 using namespace std;
 using namespace webss;
 
+/* skip junk must have its own language where
+the chars '/' and '\' are recognized as operators
+-- There are multiple languages within one language
+1) There is the main language
+2) Language of strings
+3) Language of junk
+
+There are actually 3 kinds of operators, both consisting of two chars:
+Line escape: '\' immediately followed by '\n' or whitespace or eof
+Line comment: '/' immediately followed by '/'
+Multiline comment: '/' immediately followed by '*'
+
+If '\' or '/' is met, but not an operator, then it is left
+where it was.
+
+What line escape does:
+	1) Skip current line. All chars after '\' must be whitespace, '\n', or eof
+		If not, there is an error.
+	2) Skip whitespace on line after it.
+*/
+
+bool checkOperators(SmartIterator& it);
+bool checkComment(SmartIterator& it);
+
 SmartIterator& webss::skipJunk(SmartIterator& it)
 {
-	for (; it; ++it)
-		if (!isJunk(*it))
-		{
-			if (*it == CHAR_ESCAPE)
-			{
-				if (it.peekEnd())
-					++it;
-				else if (isJunk(it.peek()))
-				{
-					checkLineEscape(++it);
-					skipJunk(it);
-				}
-			}
-			else if (*it == CHAR_COMMENT && !it.peekEnd())
-			{
-				char c = it.peek();
-				if (c == CHAR_COMMENT)
-				{
-					skipLine(it.readTwo());
-					continue;
-				}
-				else if (c == '*')
-					skipJunk(skipMultilineComment(it.readTwo()));
-			}
+	while (it)
+		if (isJunk(*it))
+			++it;
+		else if (!checkOperators(it))
 			break;
-		}
-	return it;
-}
-
-SmartIterator& webss::skipJunkToValid(SmartIterator& it)
-{
-	if (!skipJunk(it))
-		throw runtime_error(ERROR_EXPECTED);
 	return it;
 }
 
 SmartIterator& webss::skipWhitespace(SmartIterator& it)
 {
-	for (; it; ++it)
-		if (!isWhitespace(*it))
+	while (it)
+		if (isWhitespace(*it))
+			++it;
+		else
 		{
-			if (*it == CHAR_ESCAPE)
-			{
-				if (it.peekEnd())
-					++it;
-				else if (isJunk(it.peek()))
-					checkLineEscape(++it);
-			}
-			else if (*it == CHAR_COMMENT && !it.peekEnd())
-			{
-				char c = it.peek();
-				if (c == CHAR_COMMENT)
-					skipLine(it.readTwo());
-				else if (c == '*')
-					skipMultilineComment(it.readTwo());
-			}
+			checkOperators(it)
 			break;
 		}
 	return it;
@@ -70,38 +54,90 @@ SmartIterator& webss::skipWhitespace(SmartIterator& it)
 
 SmartIterator& webss::skipLine(SmartIterator& it)
 {
-	for (; it; ++it)
-		if (*it == '\n')
-			break;
+	while (it && *it != '\n')
+		++it;
 	return it;
 }
 
 SmartIterator& webss::skipMultilineComment(SmartIterator& it)
 {
-	int num = 1;
-	for (; it; ++it)
-		if (*it == '*')
+	if (!it)
+		throw runtime_error("comment is not closed");
+	
+	for (int num = 1, char c = *it; ++it; c = *it)
+		if (c == '*')
 		{
-			if (!(++it))
-				break;
-			if (*it == '/' && --num == 0)
-				return skipWhitespace(++it);
+			if (*it == '/')
+			{
+				if (--num == 0)
+					return skipWhitespace(++it);
+				if (!++it)
+					break;
+			}
 		}
-		else if (*it == '/')
+		else if (c == '/' && *it == '*')
 		{
-			if (!(++it))
+			++num;
+			if (!++it)
 				break;
-			if (*it == '*')
-				++num;
 		}
+	
 	throw runtime_error("comment is not closed");
 }
 
-void webss::cleanLine(SmartIterator& it, ConType con, Language lang)
+bool checkOperators(SmartIterator& it)
 {
-	if (!skipWhitespace(it) || isLineEnd(*it, con, lang))
-		return;
-	throw runtime_error(ERROR_UNEXPECTED);
+	if (*it == CHAR_ESCAPE)
+		return checkLineEscape(it);
+	else if (*it == CHAR_COMMENT)
+		return checkComment(it);
+	return false;
+}
+
+bool webss::checkLineEscape(SmartIterator& it)
+{
+	//'\\' + eof
+	if (it.peekEnd())
+	{
+		++it;
+		return true;
+	}
+	
+	char c = it.peek();
+	if (c != '\n' && !isWhitespace(c))
+		return false;
+	
+	//'\\' + '\n' or whitespace; whitespace must be skipped until '\n' or end of buffer
+	while (++it && isWhitespace(*it))
+		;
+	if (!it)
+		return true;
+	if (*it != '\n')
+		throw runtime_error(ERROR_UNEXPECTED);
+	skipWhitespace(++it);
+	return true;
+}
+
+bool checkComment(SmartIterator& it)
+{
+	if (it.peekEnd())
+		return false;
+	
+	char c = it.peek();
+	if (c == CHAR_COMMENT)
+		skipLine(it.readTwo());
+	else if (c == '*')
+		skipMultilineComment(it.readTwo());
+	else
+		return false;
+	return true;
+}
+
+SmartIterator& webss::skipJunkToValid(SmartIterator& it)
+{
+	if (!skipJunk(it))
+		throw runtime_error(ERROR_EXPECTED);
+	return it;
 }
 
 SmartIterator& webss::skipJunkToValidCondition(SmartIterator& it, function<bool()> condition)
@@ -112,16 +148,9 @@ SmartIterator& webss::skipJunkToValidCondition(SmartIterator& it, function<bool(
 	return it;
 }
 
-void webss::checkLineEscape(SmartIterator& it)
+void webss::cleanLine(SmartIterator& it, ConType con, Language lang)
 {
-	while (it && isWhitespace(*it))
-		++it;
-	if (!it)
+	if (!skipWhitespace(it) || isLineEnd(*it, con, lang))
 		return;
-	if (*it == '\n')
-	{
-		skipWhitespace(++it);
-		return;
-	}
 	throw runtime_error(ERROR_UNEXPECTED);
 }
