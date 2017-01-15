3205bede7f8f93b7956277ecc27aeddc0ba01efa
Pat-Laugh <p.laughrea@gmail.com>
2017-01-14 19:16:50 -0500

    no need for inheritance + tested for performance; iterator on string was particularly slow

--- WebssonParser/iterators.h
+++ WebssonParser/iterators.h
@@ -3,6 +3,8 @@
 #pragma once
 
 #include <istream>
+#include <sstream>
+#include <string>
 
 #define GET_LINE
 
@@ -12,34 +14,75 @@ namespace webss
 	class This
 	{
 	public:
-		virtual This& operator++() = 0; //prefix; no postfix because of C++ design limitations forcing the usage of CRTP idiom, which forces usage of a template, which is annoying
-		virtual This& readTwo() = 0;
-		virtual char operator*() const = 0;
+		This(const std::istream& in)
+		{
+			this->in << in.rdbuf();
+			readStart();
+		}
+		This(const std::stringstream& in)
+		{
+			this->in << in.rdbuf();
+			readStart();
+		}
+		This(const std::string& in) : in(in)
+		{
+			readStart();
+		}
+		~This() {}
 
-		virtual bool good() const { return !end(); }
-		virtual bool end() const = 0;
+		This& operator++() //prefix
+		{
+#ifdef GET_LINE
+			checkChar(c1);
+#endif
+			if (hasPeek)
+			{
+				c1 = c2;
+				getPeek();
+			}
+			else
+				isValid = false;
+			return *this;
+		}
+		This& readTwo()
+		{
+#ifdef GET_LINE
+			checkChar(c1);
+			checkChar(c2);
+#endif
+			readStart();
+			return *this;
+		}
+		char operator*() const { return c1; }
 
-		virtual operator bool() const { return good(); }
-		virtual bool operator!() const { return end(); }
+		bool good() const { return isValid; }
+		bool end() const { return !good(); }
 
-		virtual bool operator==(char c) const { return good() && c == operator*(); }
-		virtual bool operator!=(char c) const { return !operator==(c); }
+		operator bool() const { return good(); }
+		bool operator!() const { return end(); }
 
-		virtual char peek() const = 0;
-		virtual bool peekGood() const { return !peekEnd(); }
-		virtual bool peekEnd() const = 0;
+		bool operator==(char c) const { return good() && c == operator*(); }
+		bool operator!=(char c) const { return !operator==(c); }
+
+		char peek() { return c2; }
+		bool peekGood() { return hasPeek; }
+		bool peekEnd() { return !peekGood(); }
 
 #ifdef GET_LINE
 		int getLine() { return line; }
 		int getCharCol() { return col; }
-	protected:
-		int line = 1;
-		int col = 0;
+#endif
+	private:
+		std::stringstream in;
+		char c1, c2;
+		bool isValid, hasPeek;
+#ifdef GET_LINE
+		int line = 1, col = 1;
 
 		void addLine()
 		{
 			++line;
-			col = 0;
+			col = 1;
 		}
 
 		void checkChar(char c)
@@ -50,98 +93,19 @@ namespace webss
 				++col;
 		}
 #endif
-	};
-#undef This
 
-#define This SmartStreamIterator
-	class This : public SmartIterator
-	{
-	public:
-		This(std::istream& in) : in(in)
-		{
-#ifdef GET_LINE
-			c2 = 0;
-#endif
-			readTwo();
-		}
-		~This() {}
-
-		This& operator++() override
-		{
-			c1 = c2;
-			s1 = s2;
-#ifdef GET_LINE
-			if (good())
-				checkChar(c1);
-#endif
-			c2 = in.get();
-			s2 = in.rdstate();
-			return *this;
-		} //prefix
-
-		This& readTwo() override
+		void readStart()
 		{
 			c1 = in.get();
-			s1 = in.rdstate();
-#ifdef GET_LINE
-			if (peekGood())
-				checkChar(c2);
-			if (good())
-				checkChar(c1);
-#endif
+			isValid = in.rdstate() == 0;
+			getPeek();
+		}
+
+		void getPeek()
+		{
 			c2 = in.get();
-			s2 = in.rdstate();
-			return *this;
+			hasPeek = in.rdstate() == 0;
 		}
-
-		char operator*() const override { return c1; }
-		bool end() const override { return s1 != 0; }
-		char peek() const override { return c2; }
-		bool peekEnd() const override { return s2 != 0; }
-	private:
-		char c1, c2;
-		std::istream& in;
-		std::ios_base::iostate s1, s2;
-	};
-#undef This
-
-#define This SmartStringIterator
-	class This : public SmartIterator
-	{
-	public:
-		This(const std::string& in) : in(in), it(in.begin())
-		{
-#ifdef GET_LINE
-			if (good())
-				checkChar(*it);
-#endif
-		}
-		~This() {}
-
-		This& operator++() override
-		{
-			++it;
-#ifdef GET_LINE
-			if (good())
-				checkChar(*it);
-#endif
-			return *this;
-		} //prefix
-
-		This& readTwo() override
-		{
-			operator++();
-			operator++();
-			return *this;
-		}
-
-		char operator*() const override { return *it; }
-		bool end() const override { return it == in.end(); }
-		char peek() const override { return *(it + 1); }
-		bool peekEnd() const override { return (it + 1) == in.end(); }
-	private:
-		const std::string& in;
-		std::string::const_iterator it;
 	};
 #undef This
 
