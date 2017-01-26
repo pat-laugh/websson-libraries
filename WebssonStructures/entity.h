7737fe50194e65cd84236239e06bb07412301e61
Pat-Laugh <p.laughrea@gmail.com>
2017-01-26 15:55:52 -0500

    entity now a single shared pointer to an entity body

--- WebssonStructures/entity.h
+++ WebssonStructures/entity.h
@@ -17,22 +17,24 @@ namespace webss
 	{
 	public:
 		BasicEntity() {}
-		BasicEntity(std::string&& name, T&& content) : ptrName(new std::string(std::move(name))), ptrContent(new T(std::move(content))) {}
-		BasicEntity(const std::string& name, const T& content) : ptrName(new std::string(name)), ptrContent(new T(content)) {}
+		BasicEntity(std::string&& name, T&& content) : ptr(new EntityBody{ std::move(name), std::move(content) }) {}
+		BasicEntity(const std::string& name, const T& content) : ptr(new EntityBody{ name, content }) {}
 
-		const std::string& getName() const { return *ptrName; }
-		const T& getContent() const { return *ptrContent; }
+		const std::string& getName() const { return ptr->name; }
+		const T& getContent() const { return ptr->content; }
 
-		BasicEntity copyContent(std::string&& newName) const { return BasicEntity(*this, std::move(newName)); }
-
-		bool hasNamespace() const { return ptrNamespace.get() != nullptr; }
-		const BasicNamespace<T>& getNamespace() const { return *ptrNamespace; }
-		void setNamespace(const std::shared_ptr<BasicNamespace<T>>& nspace) { ptrNamespace = nspace; }
+		bool hasNamespace() const { return ptr->nspace.get() != nullptr; }
+		const BasicNamespace<T>& getNamespace() const { return *ptr->nspace; }
+		void setNamespace(const std::shared_ptr<BasicNamespace<T>>& nspace) { ptr->nspace = nspace; }
 	private:
-		std::shared_ptr<std::string> ptrName;
-		std::shared_ptr<T> ptrContent;
-		std::shared_ptr<BasicNamespace<T>> ptrNamespace;
+		class EntityBody
+		{
+		public:
+			std::string name;
+			T content;
+			std::shared_ptr<BasicNamespace<T>> nspace;
+		};
 
-		BasicEntity(const BasicEntity& ent, std::string&& newName) : ptrName(new std::string(std::move(newName))), ptrContent(ent.ptrContent) {}
+		std::shared_ptr<EntityBody> ptr;
 	};
 }
