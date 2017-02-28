cedd76bac4db0af65fa7537f385dac422210db84
Pat-Laugh <p.laughrea@gmail.com>
2017-02-27 22:23:06 -0500

    Refactoring of utilsNumbers
    Most notable, switch statements were replaced with if else statements.
    
    Overflow when parsing integers now throws an overflow_error exception. An
    error while parsing a double (infinite or NaN) still throws runtime_error.

--- WebssonParser/utilsNumbers.cpp
+++ WebssonParser/utilsNumbers.cpp
@@ -38,7 +38,7 @@ WebssInt getNumber(SmartIterator& it, NumberBase base, const function<bool(char 
 	}
 	catch (out_of_range e)
 	{
-		throw runtime_error("integer is outside bounds");
+		throw overflow_error("integer is outside bounds");
 	}
 }
 
@@ -66,57 +66,37 @@ WebssInt webss::parseIntHex(SmartIterator& it) { return getNumber(it, NumberBase
 
 WebssInt webss::parseInt(SmartIterator& it, NumberBase base)
 {
-	switch (base)
-	{
-	case NumberBase::Bin:
-		return parseIntBin(it);
-	case NumberBase::Oct:
-		return parseIntOct(it);
-	case NumberBase::Dec:
+	if (base == NumberBase::Dec)
 		return parseIntDec(it);
-	case NumberBase::Hex:
+	else if (base == NumberBase::Hex)
 		return parseIntHex(it);
-	default:
-		assert(false); throw domain_error("");
-	}
+	else if (base == NumberBase::Bin)
+		return parseIntBin(it);
+	assert(base == NumberBase::Oct);
+	return parseIntOct(it);
 }
 
 double webss::parseDecimals(SmartIterator& it, NumberBase base)
 {
 	//max digit numbers: http://stackoverflow.com/questions/17244898/maximum-number-of-decimal-digits-that-can-affect-a-double#17245451
 	//floor(<MantissaDouble(52)> * log 2 / log <base>) + 2
-	switch (base)
-	{
-	case NumberBase::Bin:
-		return checkDecimals(it, base, 54, isDigitBin, binToInt);
-	case NumberBase::Oct:
-		return checkDecimals(it, base, 19, isDigitOct, octToInt);
-	case NumberBase::Dec:
+	if (base == NumberBase::Dec)
 		return checkDecimals(it, base, 17, isDigitDec, decToInt);
-	case NumberBase::Hex:
+	else if (base == NumberBase::Hex)
 		return checkDecimals(it, base, 15, isDigitHex, hexToInt);
-	default:
-		assert(false); throw domain_error("");
-	}
+	else if (base == NumberBase::Bin)
+		return checkDecimals(it, base, 54, isDigitBin, binToInt);
+	assert(base == NumberBase::Oct);
+	return checkDecimals(it, base, 19, isDigitOct, octToInt);
 }
 
 double webss::addExponent(SmartIterator& it, double num, NumberBase base)
 {
+	assert(base == NumberBase::Dec || base == NumberBase::Hex || base == NumberBase::Bin || base == NumberBase::Oct);
 	bool negative = checkNumberNegative(it);
 	auto numBase = parseIntDec(it);
 	double exp = negative ? -numBase : numBase;
-	switch (base)
-	{
-	case NumberBase::Dec:
-		num *= pow((double)NumberBase::Dec, exp);
-		break;
-	case NumberBase::Bin: case NumberBase::Oct: case NumberBase::Hex:
-		num *= pow((double)NumberBase::Bin, exp);
-		break;
-	default:
-		assert(false); throw domain_error("");
-	}
-
+	num *= pow((double)(base == NumberBase::Dec ? base : NumberBase::Bin), exp);
 	if (!std::isfinite(num))
 		throw runtime_error("invalid number, either infinite or NaN");
 	return num;
@@ -157,35 +137,23 @@ NumberBase webss::checkNumberBase(SmartIterator& it)
 	}
 }
 
-bool webss::isDecimalSeparator(char c, Language lang)
-{
-	switch (lang)
-	{
-	case DEFAULT: case EN:
-		return c == '.';
-	case FR:
-		return c == ',';
-	case INTL:
-		return c == '.' || c == ',';
-	default:
-		assert(false); throw domain_error("");
-	}
-}
-
 bool webss::isNumberEnd(char c, Language lang, NumberBase base)
 {
 	return !isDecimalSeparator(c, lang) && !isBaseSeparator(c, base);
 }
 
+bool webss::isDecimalSeparator(char c, Language lang)
+{
+	assert(lang == DEFAULT || lang == INTL || lang == EN || lang == FR);
+	if (c == '.')
+		return lang == DEFAULT || lang == INTL || lang == EN;
+	return c == ',' && (lang == INTL || lang == FR);
+}
+
 bool webss::isBaseSeparator(char c, NumberBase base)
 {
-	switch (base)
-	{
-	case NumberBase::Dec:
+	if (base == NumberBase::Dec)
 		return c == 'e' || c == 'E';
-	case NumberBase::Bin: case NumberBase::Oct: case NumberBase::Hex:
-		return c == 'p' || c == 'P';
-	default:
-		assert(false); throw domain_error("");
-	}
+	assert(base == NumberBase::Hex || base == NumberBase::Bin || base == NumberBase::Oct);
+	return c == 'p' || c == 'P';
 }
