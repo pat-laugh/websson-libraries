93e0fe53f2b1dfa6309e6501e8d95e8881c6c99a
Pat-Laugh <p.laughrea@gmail.com>
2017-05-03 21:22:03 +0000

    Refactoring Binary Iterator
    Creation of translation unit file to put most of header code in.

---
+++ parser/binaryIterator.cpp
@@ -0,0 +1,116 @@
+//MIT License
+//Copyright(c) 2017 Patrick Laughrea
+#include "binaryIterator.h"
+
+#include <assert.h>
+#include <stdexcept>
+
+#include "utils/stringBuilder.h"
+
+using namespace std;
+using namespace webss;
+
+int BinaryIterator::getBitmask(int num)
+{
+	switch (num)
+	{
+	case 1: return 0b00000001;
+	case 2: return 0b00000011;
+	case 3: return 0b00000111;
+	case 4: return 0b00001111;
+	case 5: return 0b00011111;
+	case 6: return 0b00111111;
+	case 7: return 0b01111111;
+	case 8: return 0b11111111;
+#ifdef assert
+	default:
+		assert(false);
+		throw domain_error("");
+#endif
+	}
+}
+
+void BinaryIterator::checkBitshift()
+{
+	if (bitshift == 8)
+	{
+		byteBlock = readByte();
+		bitshift = 0;
+	}
+}
+	
+BinaryIterator::BinaryIterator(SmartIterator& it) : it(it) {}
+
+int BinaryIterator::readBit()
+{
+	checkBitshift();
+	int bit = (byteBlock >> bitshift) & 1;
+	++bitshift;
+	return bit;
+}
+
+int BinaryIterator::readBits(int numBits)
+{
+	checkBitshift();
+
+	if (bitshift + numBits <= 8)
+	{
+		int bits = (byteBlock >> bitshift) & getBitmask(numBits);
+		bitshift += numBits;
+		return bits;
+	}
+
+	//the number is spread on two bytes
+	int numBitsPart1 = 8 - bitshift, numBitsPart2 = numBits - numBitsPart1;
+	int bits = (byteBlock >> bitshift) & getBitmask(numBitsPart1);
+	byteBlock = readByte();
+	bits <<= numBitsPart2; //make space for part 2
+	bits |= (byteBlock & getBitmask(numBitsPart2));
+	bitshift = numBitsPart2;
+	return bits;
+}
+
+string::size_type BinaryIterator::readNumber()
+{
+	const int fullShift = 7, maxFullShifts = sizeof(string::size_type) / fullShift;
+	string::size_type num = 0;
+	int numShifts = 0;
+readLoop:
+	readByte();
+	num = (num << fullShift) | (0x7F & *it);
+	if ((*it & 0x80) == 0)
+		return num;
+	if (++numShifts == maxFullShifts)
+	{
+		const int partShift = sizeof(string::size_type) % fullShift;
+		const int partMask = 0xFF >> (8 - partShift);
+		const int partEndMask = 0xFF ^ partMask;
+		readByte();
+		if ((*it & partEndMask) != 0)
+			throw runtime_error("binary length is too great");
+		num = (num << partShift) | (partMask & *it);
+		return num;
+	}
+	goto readLoop;
+}
+
+void BinaryIterator::readBytes(string::size_type num, char* value)
+{
+	while (num-- > 0)
+		*value++ = readByte();
+}
+
+inline char BinaryIterator::readByte()
+{
+	if (!++it)
+		throw runtime_error("expected character");
+	return *it;
+}
+
+string BinaryIterator::readString(string::size_type num)
+{
+	StringBuilder sb;
+	while (num-- > 0)
+		sb += readByte();
+	return sb;
+}
--- parser/binaryIterator.h
+++ parser/binaryIterator.h
@@ -2,132 +2,39 @@
 //Copyright(c) 2017 Patrick Laughrea
 #pragma once
 
-#include <assert.h>
-#include <stdexcept>
-
 #include "utils/iterators.h"
-#include "utils/stringBuilder.h"
 
 namespace webss
 {
-#define This BinaryIterator
-	class This
+	class BinaryIterator
 	{
 	private:
 		SmartIterator& it;
 		char bitshift = 8;
 		char byteBlock;
 
-		int getBitmask(int num)
-		{
-			switch (num)
-			{
-			case 1: return 0b00000001;
-			case 2: return 0b00000011;
-			case 3: return 0b00000111;
-			case 4: return 0b00001111;
-			case 5: return 0b00011111;
-			case 6: return 0b00111111;
-			case 7: return 0b01111111;
-			case 8: return 0b11111111;
-#ifdef assert
-			default:
-				assert(false);
-				throw std::domain_error("");
-#endif
-			}
-		}
+		int getBitmask(int num);
 
-		void checkBitshift()
-		{
-			if (bitshift == 8)
-			{
-				byteBlock = readByte();
-				bitshift = 0;
-			}
-		}
+		void checkBitshift();
 	public:
-		This(SmartIterator& it) : it(it) {}
+		BinaryIterator(SmartIterator& it);
 
-		int readBit()
-		{
-			checkBitshift();
-			int bit = (byteBlock >> bitshift) & 1;
-			++bitshift;
-			return bit;
-		}
+		int readBit();
 
 		//numBits must be <= 8 and > 0
-		int readBits(int numBits)
-		{
-			checkBitshift();
-
-			if (bitshift + numBits <= 8)
-			{
-				int bits = (byteBlock >> bitshift) & getBitmask(numBits);
-				bitshift += numBits;
-				return bits;
-			}
-
-			//the number is spread on two bytes
-			int numBitsPart1 = 8 - bitshift, numBitsPart2 = numBits - numBitsPart1;
-			int bits = (byteBlock >> bitshift) & getBitmask(numBitsPart1);
-			byteBlock = readByte();
-			bits <<= numBitsPart2; //make space for part 2
-			bits |= (byteBlock & getBitmask(numBitsPart2));
-			bitshift = numBitsPart2;
-			return bits;
-		}
+		int readBits(int numBits);
 
 		//reads a number following UTF-7 encoding thing
-		std::string::size_type readNumber()
-		{
-			const int fullShift = 7, maxFullShifts = sizeof(std::string::size_type) / fullShift;
-			std::string::size_type num = 0;
-			int numShifts = 0;
-		readLoop:
-			readByte();
-			num = (num << fullShift) | (0x7F & *it);
-			if ((*it & 0x80) == 0)
-				return num;
-			if (++numShifts == maxFullShifts)
-			{
-				const int partShift = sizeof(std::string::size_type) % fullShift;
-				const int partMask = 0xFF >> (8 - partShift);
-				const int partEndMask = 0xFF ^ partMask;
-				readByte();
-				if ((*it & partEndMask) != 0)
-					throw std::runtime_error("binary length is too great");
-				num = (num << partShift) | (partMask & *it);
-				return num;
-			}
-			goto readLoop;
-		}
+		std::string::size_type readNumber();
 
 		//reads num number of bytes and puts them in the char pointer passed as parameter
 		//if the end of it is reached before all the bytes have been read, an error is thrown
 		//advances it past the last byte read
 		//REQUIREMENT: the char pointer must point to sufficient memory space
-		void readBytes(std::string::size_type num, char* value)
-		{
-			while (num-- > 0)
-				*value++ = readByte();
-		}
+		void readBytes(std::string::size_type num, char* value);
 
-		inline char readByte()
-		{
-			if (!++it)
-				throw std::runtime_error("expected character");
-			return *it;
-		}
+		inline char readByte();
 
-		std::string readString(std::string::size_type num)
-		{
-			StringBuilder sb;
-			while (num-- > 0)
-				sb += readByte();
-			return sb;
-		}
+		std::string readString(std::string::size_type num);
 	};
-#undef This
 }
