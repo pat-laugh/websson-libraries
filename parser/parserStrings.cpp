0ba15d8d38bac6ea43c02a81b79bb4cacc5d12d6
Patrick Laughrea <patlaughweb@gmail.com>
2017-05-16 16:45:18 +0000

    Refactoring parserStrings
    All StringBuilder variables had their names changed to "sb".

--- parser/parserStrings.cpp
+++ parser/parserStrings.cpp
@@ -12,11 +12,11 @@
 using namespace std;
 using namespace webss;
 
-void checkEscapedChar(SmartIterator& it, StringBuilder& line);
+void checkEscapedChar(SmartIterator& it, StringBuilder& sb);
 
-inline void putChar(SmartIterator& it, StringBuilder& s)
+inline void putChar(SmartIterator& it, StringBuilder& sb)
 {
-	s += *it;
+	sb += *it;
 	++it;
 }
 
@@ -25,7 +25,7 @@ bool isEnd(SmartIterator& it, function<bool()> endCondition)
 	return !it || *it == '\n' || endCondition();
 }
 
-bool hasNextChar(SmartIterator& it, StringBuilder& line, function<bool()> endCondition = []() { return false; })
+bool hasNextChar(SmartIterator& it, StringBuilder& sb, function<bool()> endCondition = []() { return false; })
 {
 	if (isEnd(it, endCondition))
 		return false;
@@ -43,7 +43,7 @@ bool hasNextChar(SmartIterator& it, StringBuilder& line, function<bool()> endCon
 		if (checkJunkOperators(it) && isEnd(it, endCondition))
 			return false;
 		while (spaces-- > 0)
-			line += ' ';
+			sb += ' ';
 	}
 	
 	return true;
@@ -52,45 +52,45 @@ bool hasNextChar(SmartIterator& it, StringBuilder& line, function<bool()> endCon
 string Parser::parseLineString()
 {
 	skipLineJunk(it);
-	StringBuilder line;
+	StringBuilder sb;
 	if (multilineContainer)
 	{
-		while (hasNextChar(it, line))
+		while (hasNextChar(it, sb))
 		{
 			if (*it == CHAR_ESCAPE)
 			{
-				checkEscapedChar(it, line);
+				checkEscapedChar(it, sb);
 				continue;
 			}
-			else if (*it == CHAR_CONCRETE_ENTITY && checkStringEntity(line))
+			else if (*it == CHAR_CONCRETE_ENTITY && checkStringEntity(sb))
 				continue;
-			putChar(it, line);
+			putChar(it, sb);
 		}
 	}
 	else
 	{
 		int countStartEnd = 1;
-		while (hasNextChar(it, line, [&]() { return *it == CHAR_SEPARATOR || (con.isEnd(*it) && --countStartEnd == 0); }))
+		while (hasNextChar(it, sb, [&]() { return *it == CHAR_SEPARATOR || (con.isEnd(*it) && --countStartEnd == 0); }))
 		{
 			if (*it == CHAR_ESCAPE)
 			{
-				checkEscapedChar(it, line);
+				checkEscapedChar(it, sb);
 				continue;
 			}
-			else if (*it == CHAR_CONCRETE_ENTITY && checkStringEntity(line))
+			else if (*it == CHAR_CONCRETE_ENTITY && checkStringEntity(sb))
 				continue;
 			else if (con.isStart(*it))
 				++countStartEnd;
-			putChar(it, line);
+			putChar(it, sb);
 		}
 	}
-	return line;
+	return sb;
 }
 
 string Parser::parseMultilineString()
 {
 	ContainerSwitcher switcher(*this, ConType::DICTIONARY, true);
-	StringBuilder text;
+	StringBuilder sb;
 	if (*skipJunkToValid(it) == CLOSE_DICTIONARY)
 		return "";
 
@@ -99,42 +99,42 @@ string Parser::parseMultilineString()
 loopStart:
 	if (multilineContainer)
 	{
-		while (hasNextChar(it, text))
+		while (hasNextChar(it, sb))
 		{
 			if (*it == CHAR_ESCAPE)
 			{
-				checkEscapedChar(it, text);
+				checkEscapedChar(it, sb);
 				addSpace = false;
 				continue;
 			}
-			else if (*it == CHAR_CONCRETE_ENTITY && checkStringEntity(text))
+			else if (*it == CHAR_CONCRETE_ENTITY && checkStringEntity(sb))
 			{
 				addSpace = true;
 				continue;
 			}
 			addSpace = true;
-			putChar(it, text);
+			putChar(it, sb);
 		}
 	}
 	else
 	{
-		while (hasNextChar(it, text, [&]() { return *it == CLOSE_DICTIONARY && --countStartEnd == 0; }))
+		while (hasNextChar(it, sb, [&]() { return *it == CLOSE_DICTIONARY && --countStartEnd == 0; }))
 		{
 			if (*it == CHAR_ESCAPE)
 			{
-				checkEscapedChar(it, text);
+				checkEscapedChar(it, sb);
 				addSpace = false;
 				continue;
 			}
 			else if (*it == OPEN_DICTIONARY)
 				++countStartEnd;
-			else if (*it == CHAR_CONCRETE_ENTITY && checkStringEntity(text))
+			else if (*it == CHAR_CONCRETE_ENTITY && checkStringEntity(sb))
 			{
 				addSpace = true;
 				continue;
 			}
 			addSpace = true;
-			putChar(it, text);
+			putChar(it, sb);
 		}
 	}
 	if (!it)
@@ -149,17 +149,17 @@ loopStart:
 			break;
 	case 0:
 		++it;
-		return text;
+		return sb;
 	}
 	if (addSpace)
-		text += ' ';
+		sb += ' ';
 	goto loopStart;
 }
 
 string Parser::parseCString()
 {
 	++it;
-	StringBuilder cstr;
+	StringBuilder sb;
 	while (it)
 	{
 		switch (*it)
@@ -168,25 +168,25 @@ string Parser::parseCString()
 			throw runtime_error("can't have line break in cstring");
 		case CHAR_CSTRING:
 			++it;
-			return cstr;
+			return sb;
 		case CHAR_CONCRETE_ENTITY:
-			if (checkStringEntity(cstr))
+			if (checkStringEntity(sb))
 				continue;
 			break;
 		case CHAR_ESCAPE:
-			checkEscapedChar(it, cstr);
+			checkEscapedChar(it, sb);
 			continue;
 		default:
 			break;
 		}
 		if (!isControlAscii(*it))
-			cstr += *it;
+			sb += *it;
 		++it;
 	}
 	throw runtime_error("cstring is not closed");
 }
 
-void checkEscapedChar(SmartIterator& it, StringBuilder& line)
+void checkEscapedChar(SmartIterator& it, StringBuilder& sb)
 {
 	if (!++it)
 		throw runtime_error(ERROR_EXPECTED);
@@ -194,35 +194,35 @@ void checkEscapedChar(SmartIterator& it, StringBuilder& line)
 	switch (*it)
 	{
 	case 'x': case 'u': case 'U':
-		putEscapedHex(it, line);
+		putEscapedHex(it, sb);
 		return;
 
-	case '0': line += '\0'; break;
-    case 'a': line += '\a'; break;
-    case 'b': line += '\b'; break;
+	case '0': sb += '\0'; break;
+    case 'a': sb += '\a'; break;
+    case 'b': sb += '\b'; break;
 	case 'e': /* empty */ break;
-    case 'f': line += '\f'; break;
-	case 'n': line += '\n'; break;
-	case 'r': line += '\r'; break;
-	case 's': line += ' '; break;
-	case 't': line += '\t'; break;
-    case 'v': line += '\v'; break;
+    case 'f': sb += '\f'; break;
+	case 'n': sb += '\n'; break;
+	case 'r': sb += '\r'; break;
+	case 's': sb += ' '; break;
+	case 't': sb += '\t'; break;
+    case 'v': sb += '\v'; break;
 	default:
 		if (!isSpecialAscii(*it))
 			throw runtime_error("invalid char escape");
-		line += *it;
+		sb += *it;
 		break;
 	}
 	++it;
 }
 
-bool Parser::checkStringEntity(StringBuilder& line)
+bool Parser::checkStringEntity(StringBuilder& sb)
 {
 	if (it.peekEnd() || !isNameStart(it.peek()))
 		return false;
 
 	++it;
-	line += parseStringEntity();
+	sb += parseStringEntity();
 	return true;
 }
 
