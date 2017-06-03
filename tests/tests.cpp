a4f0b382a53eba917428d5980519b9d96bc4d015
Patrick Laughrea <patlaughweb@gmail.com>
2017-06-03 15:44:03 +0000

    Add tests for dictionary

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -10,10 +10,14 @@
 using namespace std;
 using namespace webss;
 
+string makeCompleteFilenameIn(string filename);
+string makeCompleteFilenameOut(string filename);
+void testDictionary();
+
+char inChar;
+
 int main()
 {
-	char inChar;
-
 //#define TEST_PERFORMANCE
 #ifdef TEST_PERFORMANCE
 	for (int i = 0; i < 2000; ++i)
@@ -71,9 +75,73 @@ int main()
 #ifdef TEST_PERFORMANCE
 	}
 #else
+		testDictionary();
 		cin >> inChar;
 	} while (inChar != 'q');
 #endif
 
     return 0;
+}
+
+string makeCompleteFilenameIn(string filename)
+{
+	return "files-in/" + filename + ".wbsn";
+}
+string makeCompleteFilenameOut(string filename)
+{
+	return filename + ".wbsnout";
+}
+
+void testDictionary()
+{
+	string filename("dictionary");
+	Document data;
+	string filenameIn(makeCompleteFilenameIn(filename));
+	string filenameOut(makeCompleteFilenameOut(filename));
+
+	cout << "Input: " << filenameIn << endl;
+
+	ifstream fileIn(filenameIn, ios::binary);
+	if (fileIn.fail()) { cerr << "Error: failed to open file \"" << filenameIn << "\"" << endl; cin >> inChar; exit(EXIT_FAILURE); }
+	try
+	{
+		Parser parser(fileIn);
+		data = parser.parseDocument();
+
+		const auto& webssDict = data.getData()[0];
+		assert(webssDict.isDictionary());
+		const auto& dict = webssDict.getDictionary();
+		assert(dict.size() == 4);
+		assert(dict.has("a_list"));
+		assert(dict.has("key1"));
+		assert(dict.has("key2"));
+		assert(dict.has("other-dict"));
+		assert(dict["a_list"].isList() && dict["a_list"].getList().size() == 4);
+		assert((int)dict["a_list"][0] == 0 && (int)dict["a_list"][1] == 1 && (int)dict["a_list"][2] == 2 && (int)dict["a_list"][3] == 3);
+		assert(dict["key1"].isInt() && dict["key1"].getInt() == 2);
+		assert(dict["key2"].isString() && dict["key2"].getString() == "text");
+		assert(dict["other-dict"].isDictionary() && dict["other-dict"].getDictionary().has("key") && dict["other-dict"]["key"].isBool() && dict["other-dict"]["key"].getBool() == true);
+
+		cout << "No errors while parsing" << endl;
+
+		ofstream fileOut(filenameOut, ios::binary);
+		if (fileOut.fail()) { cerr << "Error: failed to open file \"" << filenameOut << "\"" << endl; cin >> inChar; exit(EXIT_FAILURE); }
+		try
+		{
+			auto output = Serializer::serialize(data);
+			fileOut << output;
+			fileOut.flush();
+			cout << "No errors while serializing" << endl;
+		}
+		catch (const exception& e)
+		{
+			cout << "Serialization failed: " << e.what() << endl;
+		}
+		fileOut.close();
+	}
+	catch (const exception& e)
+	{
+		cout << "Parse failed: " << e.what() << endl;
+	}
+	fileIn.close();
 }
