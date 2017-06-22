4a541f755c1eeb2bde67451316627e917d790128
Patrick Laughrea <patlaughweb@gmail.com>
2017-06-22 12:55:11 +0000

    Improve String Builder
    There were some bugs in it; its behavior has now been thoroughly tested.

--- utils/stringBuilder.hpp
+++ utils/stringBuilder.hpp
@@ -7,20 +7,31 @@
 
 namespace webss
 {
-#define This StringBuilder
-	class This
+	class StringBuilder
 	{
 	public:
-		This& operator+=(char c) { ss.put(c); return *this; }
-		This& operator+=(std::string&& s) { ss << std::move(s); return *this; }
-		This& operator+=(const std::string& s) { ss << s; return *this; }
-		This& operator+=(const char* s) { ss << s; return *this; }
-		This& operator+=(const This& o) { ss << o.ss.rdbuf(); return *this; }
+		StringBuilder() {}
+		StringBuilder(StringBuilder&& o) : ss(std::move(o.ss)) {}
+		StringBuilder(const StringBuilder& o) { put(o); }
+		StringBuilder(char c) { put(c); }
+		StringBuilder(std::string s) { put(std::move(s)); }
+		StringBuilder(const char* s) { put(s); }
+
+		StringBuilder& operator=(StringBuilder o) { ss = std::move(o.ss); return *this; }
+
+		StringBuilder& put(const StringBuilder& o) { ss << o.str(); return *this; }
+		StringBuilder& put(char c) { ss.put(c); return *this; }
+		StringBuilder& put(std::string s) { ss << std::move(s); return *this; }
+		StringBuilder& put(const char* s) { ss << s; return *this; }
+
+		StringBuilder& operator+=(const StringBuilder& o) { return put(o); }
+		StringBuilder& operator+=(char c) { return put(c); }
+		StringBuilder& operator+=(std::string s) { return put(std::move(s)); }
+		StringBuilder& operator+=(const char* s) { return put(s); }
 
 		operator std::string() const { return ss.str(); }
-		std::string str() { return ss.str(); }
+		std::string str() const { return ss.str(); }
 	private:
-		std::stringstream ss;
+		std::ostringstream ss;
 	};
-#undef This
 }
