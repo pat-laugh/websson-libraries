6141351bb0da7a2361aee4c58679b62d245d3c89
Patrick Laughrea <patlaughweb@gmail.com>
2017-06-22 20:20:57 -0400

    Add Makefile and improve wbsn-to-html

--- applications/wbsn-to-html/wbsn-to-html.cpp
+++ applications/wbsn-to-html/wbsn-to-html.cpp
@@ -2,6 +2,7 @@
 //Copyright 2017 Patrick Laughrea
 
 #include <iostream>
+#include <fstream>
 
 #include "parser/parser.hpp"
 #include "serializerHtml/serializerHtml.hpp"
@@ -9,9 +10,47 @@
 using namespace std;
 using namespace webss;
 
-int main()
+int main(int argc, char** argv)
 {
-	cout << SerializerHtml::serialize(Parser(SmartIterator(cin.rdbuf())).parseDocument());
+	SmartIterator it("");
+	if (argc <= 1)
+		it = SmartIterator(cin.rdbuf());
+	else if (argc > 3)
+	{
+		cout << "Usage: " << argv[0] << " [fileIn] [fileOut]" << endl;
+		return 1;
+	}
+	else
+	{
+		ifstream fileIn(argv[1], ios::binary);
+		if (fileIn.fail())
+		{
+			cout << "Error: failed to open file \"" << argv[1] << "\"" << endl;
+			return 1;
+		}
+		it = SmartIterator(fileIn.rdbuf());
+	}
+	try
+	{
+		auto doc = Parser(move(it)).parseDocument();
+		if (argc <= 2)
+			cout << SerializerHtml::serialize(doc);
+		else
+		{
+			ofstream fileOut(argv[2], ios::binary);
+			if (fileOut.fail())
+			{
+				cout << "Error: failed to open file \"" << argv[2] << "\"" << endl;
+				return 1;
+			}
+			fileOut << SerializerHtml::serialize(doc);
+		}
+	} catch (const exception& e)
+	{
+		cerr << "Error: " << e.what() << endl;
+		return 1;
+	}
+		
     return 0;
 }
 
