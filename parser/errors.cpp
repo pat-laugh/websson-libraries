5b7940b932e5bc0d7cea0bbcf8758223f66d0a7d
Patrick Laughrea <patlaughweb@gmail.com>
2017-07-13 23:37:32 -0400

    Put parser errors definitions in source file

---
+++ parser/errors.cpp
@@ -0,0 +1,10 @@
+//MIT License
+//Copyright 2017 Patrick Laughrea
+#include "errors.hpp"
+
+const char* webss::ERROR_ANONYMOUS_KEY = "can't have anonymous key in dictionary, template head or enum";
+const char* webss::ERROR_BIN_SIZE_HEAD = "size of binary head must be a positive integer, binary template head or equivalent entity";
+const char* webss::ERROR_EXPECTED = "expected character";
+const char* webss::ERROR_EXPECTED_NUMBER = "expected number";
+const char* webss::ERROR_UNEXPECTED = "unexpected character";
+const char* webss::ERROR_VOID = "can't have void element";
