b8482ec24085a9464e8133a03b20033b9a7c1b68
Patrick Laughrea <patlaughweb@gmail.com>
2017-12-09 15:37:04 -0500

    wbsn-to-html refactor and improve error messages

--- applications/wbsn-to-html/wbsn-to-html.cpp
+++ applications/wbsn-to-html/wbsn-to-html.cpp
@@ -12,6 +12,9 @@ using namespace std;
 using namespace various;
 using namespace webss;
 
+void exitErr(const char* cmd, const char* err, const char* reason);
+void putErr(const char* cmd, const char* err, const char* reason);
+
 int main(int argc, char** argv)
 {
 	SmartIterator it("");
@@ -19,40 +22,51 @@ int main(int argc, char** argv)
 		it = SmartIterator(cin.rdbuf());
 	else if (argc > 3)
 	{
-		cout << "Usage: " << argv[0] << " [fileIn] [fileOut]" << endl;
-		return 1;
+		cerr << "Usage: " << argv[0] << " [fileIn] [fileOut]" << endl;
+		return EXIT_FAILURE;
 	}
 	else
 	{
 		ifstream fileIn(argv[1], ios::binary);
-		if (fileIn.fail())
-		{
-			cout << "Error: failed to open file \"" << argv[1] << "\"" << endl;
-			return 1;
-		}
+		if (!fileIn)
+			exitErr(argv[0], (string("failed to open file ") + argv[1]).c_str(), "error");
 		it = SmartIterator(fileIn.rdbuf());
 	}
+	
+	Document doc;
+	try
+		{ doc = Parser(move(it), argv[1]).parseDocument(); }
+	catch (const exception& e)
+		{ exitErr(argv[0], "parse failed", e.what()); }
 	try
 	{
-		auto doc = Parser(move(it), argv[1]).parseDocument();
 		if (argc <= 2)
+		{
 			cout << SerializerHtml::serialize(doc);
+			cout.flush();
+		}
 		else
 		{
 			ofstream fileOut(argv[2], ios::binary);
 			if (fileOut.fail())
-			{
-				cout << "Error: failed to open file \"" << argv[2] << "\"" << endl;
-				return 1;
-			}
+				exitErr(argv[0], (string("failed to open file ") + argv[2]).c_str(), "error");
 			fileOut << SerializerHtml::serialize(doc);
+			fileOut.flush();
 		}
-	} catch (const exception& e)
-	{
-		cerr << "Error: " << e.what() << endl;
-		return 1;
 	}
+	catch (const exception& e)
+		{ exitErr(argv[0], "serialization failed", e.what()); }
 		
     return 0;
 }
 
+void exitErr(const char* cmd, const char* err, const char* reason)
+{
+	putErr(cmd, err, reason);
+	exit(EXIT_FAILURE);
+}
+
+void putErr(const char* cmd, const char* err, const char* reason)
+{
+	cerr << cmd << ": " << err << ": " << reason << endl;
+}
